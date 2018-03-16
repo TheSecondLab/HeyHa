aa87979e336b93de2d99e48d9680fac52d71993a
jiyiwohanxing <467756960@qq.com>
2018-03-16 10:32:31 +0800

    :construction: forget to commit /(ㄒoㄒ)/~~

---
+++ HeyHaCoach/src/components/class-list/class-list.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+/**
+ * Generated class for the ClassListComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'class-list',
+  templateUrl: 'class-list.html'
+})
+export class ClassListComponent {
+
+  public list = [{
+    type: '动态',
+    time: '周六、日 14：30-16：00',
+    name: '11届精英2班（周六、日）',
+    id: 'CFTA110500'
+  }, {
+    type: '动态',
+    time: '周六、日 14：30-16：00',
+    name: '11届精英2班（周六、日）',
+    id: 'CFTA110500'
+  }, {
+    type: '动态',
+    time: '周六、日 14：30-16：00',
+    name: '11届精英2班（周六、日）',
+    id: 'CFTA110500'
+  }];
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goClassDetail(id) {
+    console.log(id);
+    // this.navCtrl.push();
+  }
+
+}
---
+++ HeyHaCoach/src/components/remind-list-all/remind-list-all.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+/**
+ * Generated class for the RemindListAllComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'remind-list-all',
+  templateUrl: 'remind-list-all.html'
+})
+export class RemindListAllComponent {
+
+  public list = [{
+    imgUrl: 'https://tac-cdn.zhongan.com/care/user_image/iphoto.JPG',
+    trace: '2',
+    date: '2019-2-2',
+    name: '金田',
+    number: 'CFTA110500'
+  }, {
+    imgUrl: 'https://tac-cdn.zhongan.com/care/user_image/iphoto.JPG',
+    trace: '2',
+    date: '2019-2-2',
+    name: '金田',
+    number: 'CFTA110500'
+  }, {
+    imgUrl: 'https://tac-cdn.zhongan.com/care/user_image/iphoto.JPG',
+    trace: '2',
+    date: '2019-2-2',
+    name: '金田',
+    number: 'CFTA110500'
+  }];
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  goRemindDetail(number) {
+    console.log(number);
+    // this.navCtrl.push();
+  }
+}
