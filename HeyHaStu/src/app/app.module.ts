a8d0879f4886c08bafdbc17f5d25f2256658267a
MeatPieYan <robinyzg@hotmail.com>
2018-03-24 18:04:37 +0800

    fix issues for HeyHaStu

--- HeyHaStu/src/app/app.module.ts
+++ HeyHaStu/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpClientModule } from "@angular/common/http";
+import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 
 // 自定义组件
@@ -8,14 +10,11 @@ import { ComponentsModule } from '../components/components.module';
 import { MyPointPageModule } from '../pages/my-point/my-point.module';
 import { CoachInfoPageModule } from '../pages/coach-info/coach-info.module';
 import { CardsPageModule } from '../pages/cards/cards.module';
-
 import { EventsPage } from '../pages/events/events';
 import { MinePage } from '../pages/mine/mine';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { TrainPage } from '../pages/train/train';
-
-import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyTaskPage } from '../pages/my-task/my-task';
 import { SettingPage } from '../pages/setting/setting';
@@ -23,9 +22,8 @@ import { CoachInfoPage } from '../pages/coach-info/coach-info';
 import { PersonalInfoPage } from '../pages/personal-info/personal-info';
 import { CardsPage } from '../pages/cards/cards';
 import { MyPointPage } from '../pages/my-point/my-point';
-
-import {HttpClientModule} from "@angular/common/http";
 import { PersonalInfoPageModule } from '../pages/personal-info/personal-info.module';
+import { SettingPageModule } from '../pages/setting/setting.module';
 
 // NgModule
 @NgModule({
@@ -36,21 +34,21 @@ import { PersonalInfoPageModule } from '../pages/personal-info/personal-info.mod
     HomePage,
     TabsPage,
     TrainPage,
-    MyTaskPage,
-    SettingPage
+    MyTaskPage
   ],
   imports: [ // 当前的项目依赖哪些组件
     BrowserModule,
     ComponentsModule,
     IonicModule.forRoot(MyApp, {
-      tabsHideOnSubPages: 'true',
-       mode: 'ios'
+      tabsHideOnSubPages: 'true'
+      //  mode: 'ios'
      }),
     HttpClientModule,
     MyPointPageModule,
     CoachInfoPageModule,
     PersonalInfoPageModule,
-    CardsPageModule
+    CardsPageModule,
+    SettingPageModule
   ],
   bootstrap: [IonicApp], // 默认启动的组件
   entryComponents: [
--- HeyHaStu/src/pages/mine/mine.ts
+++ HeyHaStu/src/pages/mine/mine.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core'; // Angular核心组件
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the MinePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -20,4 +14,7 @@ export class MinePage {
     this.i++;
   }
 
+  navTo(page) {
+    this.navCtrl.push(page);
+  }
 }
