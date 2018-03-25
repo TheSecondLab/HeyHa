7189fe4924ce444edbebf6f52e315b3b90146462
MeatPieYan <robinyzg@hotmail.com>
2018-03-25 17:35:04 +0800

    add menu

--- HeyHaCoach/src/pages/task-list/task-list.ts
+++ HeyHaCoach/src/pages/task-list/task-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, MenuController } from 'ionic-angular';
 
 /**
  * Generated class for the TaskListPage page.
@@ -15,11 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TaskListPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public menu: MenuController) {
+    menu.enable(true);
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TaskListPage');
+  showMenu() {
+    this.menu.toggle()
+  }
+
+  openPage(str) {
+    console.log(str);
+    this.menu.close();
   }
 
 }
