5ca20bec9bf674dfde4b04fe6f27c5d9f0041cd8
jiyiwohanxing <467756960@qq.com>
2018-03-29 19:17:26 +0800

    delete some cod

--- HeyHaCoach/src/app/app.component.ts
+++ HeyHaCoach/src/app/app.component.ts
@@ -28,55 +28,30 @@ export class MyApp {
       splashScreen.hide();
 
       //根据不同平台实现消息推送功能
-      console.log(this.platform.is('ios'));
-      
-
       if(this.platform.is('ios')){
         this.initPushNotification();
       }else if(this.platform.is('android')){
         this.jPush.init();
         this.androidGetRegId();
-
-        // this.jPush.openNotification()
-        // .subscribe( res => {
-        //   console.log('收到推送');
-        //   console.log(res)
-        // });
-  
-        // this.jPush.receiveNotification()
-        //   .subscribe( res => {
-        //     console.log('收到推送');
-        //     console.log(res)
-        // });
-  
-        // this.jPush.receiveMessage()
-        //   .subscribe( res => {
-        //     console.log('收到推送');
-        //     console.log(res)
-        // });
-      }else{
-        //...
       }
     });
   }
-
 
   //Android端获取RegestrationId，用于消息推送
   androidGetRegId(){
     this.jPush.getRegistrationID()
     .then(regId => {
       console.log('********', regId)
-      // this.storage.set('regId', regId);
     })
     .catch(err => alert(err))
   }
 
-  //IOS端消息推送函数
+  //ios消息推送
   initPushNotification() { 
     if(!this.platform.is('cordova')) {
       console.warn('Push notifications only work on a real device');
-        return;
-      }
+      return;
+    }
 }
 
 }
